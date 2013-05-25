0672faddbaae4901964bf6fa9f751dd3d2d3dc48
Yu-Hung Lin <yuhung@Yu-Hungs-MacBook-Pro.local>
2013-05-25 10:25:24 -0700

    truecrypt tinkering

--- modules/people/manifests/yuhunglin/applications.pp
+++ modules/people/manifests/yuhunglin/applications.pp
@@ -3,5 +3,10 @@ class people::yuhunglin::applications {
   sublime_text_2::package { 'Emmet':
     source => 'sergeche/emmet-sublime'
   }
-
+  
+  package {'Truecrypt':
+    ensure => installed,
+    source => 'http://faro.puppetlabs.lan/gary/TrueCrypt-7.1a.dmg'
+    provider => appdmg,
+  }
 }
